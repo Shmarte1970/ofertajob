d8c9bc595287472d34b57f5aeb4112b27d96b9b9
Shmarte.1970 <shmarte@gmail.com>
2022-03-14 17:44:09 +0100

    crud v1

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+
+// Settings
+app.set('port', process.env.PORT || 3000);    // Definicion del puerto del hostin s no dos da uno usamos el 3000
+
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes
+
+
+// Static files
+
+
+//Starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+
+}); 
+
